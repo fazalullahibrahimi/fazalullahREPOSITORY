d5cf4a2fbbdbcb9075ad01a6d51efe95862f9b58
fazalullahRasoli <122254861+fazalullahibrahimi@users.noreply.github.com>
2023-02-23 05:46:49 -0800

    Add files via upload

---
+++ draw squre in python to use Turtle library (1402   ,12  , 2 )   Date.py
@@ -0,0 +1,9 @@
+import turtle
+turtle.Screen()
+TH=turtle.Turtle()
+i=0 
+while(i<3):
+  i +=1
+  TH.forward(110)
+  TH.left(120)
+turtle.mainloop()
---
+++ drawe cercle in python to use Turtle library.py
@@ -0,0 +1,9 @@
+import turtle
+turtle.Screen()
+TH=turtle.Turtle()
+i=0 
+while(i<360):
+  i +=1
+  TH.forward(2)
+  TH.left(1)
+turtle.mainloop()
